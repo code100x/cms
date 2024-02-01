772f6a6f5325755eee776d81ad79ec9865566536
hkirat <harkirat96@gmail.com>
2024-02-01 23:39:25 +0100

    fixed layout

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -15,58 +15,61 @@ export const Appbar = () => {
   const session = useSession();
 
   return (
-    <nav className="fixed z-50 top-0 px-4 w-full h-16 border-b shadow-sm bg-background/80 backdrop-blur-md flex items-center">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between w-full">
-        <Logo onFooter={false} />
+    <>
+      <nav className="fixed z-50 top-0 px-4 w-full h-16 border-b shadow-sm bg-background/80 backdrop-blur-md flex items-center">
+        <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between w-full">
+          <Logo onFooter={false} />
 
-        {session?.data?.user ? (
-          <div className="flex items-center space-x-2">
-            <div className="hidden sm:flex items-center justify-around md:w-auto md:block space-x-2">
-              <Button variant={'link'} size={'sm'} asChild>
-                <JoinDiscord isNavigated={false} />
-              </Button>
+          {session?.data?.user ? (
+            <div className="flex items-center space-x-2">
+              <div className="hidden sm:flex items-center justify-around md:w-auto md:block space-x-2">
+                <Button variant={'link'} size={'sm'} asChild>
+                  <JoinDiscord isNavigated={false} />
+                </Button>
 
-              <Button size={'sm'} variant={'link'} asChild>
-                <Link
-                  href={'https://github.com/100xdevs-cohort-2/assignments'}
-                  target="_blank"
-                >
-                  Assignments
-                </Link>
-              </Button>
+                <Button size={'sm'} variant={'link'} asChild>
+                  <Link
+                    href={'https://github.com/100xdevs-cohort-2/assignments'}
+                    target="_blank"
+                  >
+                    Assignments
+                  </Link>
+                </Button>
 
-              <AppbarAuth />
+                <AppbarAuth />
+              </div>
+
+              <ThemeToggler />
+
+              <div className="block sm:hidden">
+                <NavigationMenu />
+              </div>
             </div>
+          ) : (
+            <div className="flex items-center space-x-2">
+              <div className="hidden sm:flex items-center justify-around md:w-auto md:block space-x-3">
+                <AppbarAuth />
 
-            <ThemeToggler />
+                <Button size={'sm'} asChild>
+                  <Link
+                    href={'https://harkirat.classx.co.in/new-courses'}
+                    target="_blank"
+                  >
+                    Join now <Sparkles className="ml-1 h-4 w-4" />
+                  </Link>
+                </Button>
+              </div>
 
-            <div className="block sm:hidden">
-              <NavigationMenu />
+              <ThemeToggler />
+
+              <div className="block sm:hidden">
+                <NavigationMenu />
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="flex items-center space-x-2">
-            <div className="hidden sm:flex items-center justify-around md:w-auto md:block space-x-3">
-              <AppbarAuth />
-
-              <Button size={'sm'} asChild>
-                <Link
-                  href={'https://harkirat.classx.co.in/new-courses'}
-                  target="_blank"
-                >
-                  Join now <Sparkles className="ml-1 h-4 w-4" />
-                </Link>
-              </Button>
-            </div>
-
-            <ThemeToggler />
-
-            <div className="block sm:hidden">
-              <NavigationMenu />
-            </div>
-          </div>
-        )}
-      </div>
-    </nav>
+          )}
+        </div>
+      </nav>
+      <div className="h-16 w-full" />
+    </>
   );
 };
