b80bde6aa28bd9ff895146d86d7e2e5b6fab8cef
tanayvaswani <vaswani.tanay9@gmail.com>
2024-02-04 09:14:55 +0530

    chore: color on complete progress

--- src/components/PercentageComplete.tsx
+++ src/components/PercentageComplete.tsx
@@ -1,6 +1,8 @@
 const PercentageComplete = ({ percent }: { percent: number }) => {
   return (
-    <div className="w-full text-sm flex items-center justify-center text-blue-600 py-1 px-2 rounded-full font-semibold">
+    <div
+      className={`${percent === 100 ? 'text-green-500' : 'text-red-500'} w-full text-sm flex items-center justify-center py-1 px-2 rounded-full font-semibold`}
+    >
       <div className="w-full">{`${percent}% completed`}</div>
     </div>
   );
