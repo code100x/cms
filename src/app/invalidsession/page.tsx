43cb7ec4656a037289344b21cc18db2b829119c8
Harpreet Singh <szinghh@gmail.com>
2024-02-26 14:40:04 +0530

    added ui changes

--- src/app/invalidsession/page.tsx
+++ src/app/invalidsession/page.tsx
@@ -2,15 +2,27 @@
 
 import { signOut } from 'next-auth/react';
 import React, { useEffect } from 'react';
+import { Toaster } from '@/components/ui/sonner';
+import { toast } from 'sonner';
 
 const page = () => {
   useEffect(() => {
     signOut({
       callbackUrl: '/signin',
     });
+    toast('Too many devices connected. Logging out!', {
+      action: {
+        label: 'Close',
+        onClick: () => console.log('Closed Toast'),
+      },
+    });
   }, []);
 
-  return <div>page</div>;
+  return (
+    <div>
+      <Toaster />
+    </div>
+  );
 };
 
 export default page;
