e7edb1e3d91197d794f9d4ae4b004517ab2593ce
Aadesh Kulkarni <aadeshkulkarni@Aadeshs-MacBook-Pro.local>
2024-03-10 14:49:18 +0530

    ui-fix: highlighted chapter bg and text clash in light mode

--- src/components/VideoContentChapters.tsx
+++ src/components/VideoContentChapters.tsx
@@ -44,7 +44,7 @@ const VideoContentChapters = ({
           return (
             <div key={`${index}-${start}${end}${title}`}>
               <div
-                className={`dark:text-white text-black p-2 py-3 flex items-center gap-3 justify-between cursor-pointer ${currentTime >= start && currentTime < end ? 'bg-[#27272A]' : ''}`}
+                className={`dark:text-white text-black p-2 py-3 flex items-center gap-3 justify-between cursor-pointer ${currentTime >= start && currentTime < end ? 'dark:bg-[#27272A] bg-zinc-200' : ''}`}
                 onClick={() => {
                   player.currentTime(start);
                   player.play();
