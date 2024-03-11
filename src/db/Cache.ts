924c3923eb079c90d4d42421f6e57d8b6df06d30
tanmay4l <89733575+TanmayDhobale@users.noreply.github.com>
2024-03-11 20:43:00 +0530

    Removed the unnecessary else after return as per the linting suggestion.

--- src/db/Cache.ts
+++ src/db/Cache.ts
@@ -1,41 +1,52 @@
 export class Cache {
-  private inMemoryDb: Map<string, { value: any; expiry: number; }>;
+  private inMemoryDb: Map<
+    string,
+    {
+      value: any;
+      expiry: number;
+    }
+  >;
   private static instance: Cache;
 
   private constructor() {
-    this.inMemoryDb = new Map();
+    this.inMemoryDb = new Map<
+      string,
+      {
+        value: any;
+        expiry: number;
+      }
+    >();
   }
 
-  static getInstance(): Cache {
-    if (!Cache.instance) {
-      Cache.instance = new Cache();
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Cache();
     }
-    return Cache.instance;
+
+    return this.instance;
   }
 
-  set(type: string, args: string[], value: any, expiryInSeconds: number = parseInt(process.env.CACHE_EXPIRE_S || '100', 10)): void {
-    const currentTime = new Date().getTime();
-    const expiry = currentTime + expiryInSeconds * 1000;
-    this.inMemoryDb.set(`${type} ${JSON.stringify(args)}`, { value, expiry });
+  set(type: string, args: string[], value: any, expirySeconds: number = parseInt(process.env.CACHE_EXPIRE_S || '100', 10)) {
+    this.inMemoryDb.set(`${type} ${JSON.stringify(args)}`, {
+      value,
+      expiry: new Date().getTime() + expirySeconds * 1000,
+    });
   }
 
-  get(type: string, args: string[]): any | null {
+  get(type: string, args: string[]) {
     const key = `${type} ${JSON.stringify(args)}`;
-    const cacheEntry = this.inMemoryDb.get(key);
-    if (cacheEntry && cacheEntry.expiry > new Date().getTime()) {
-        return cacheEntry.value;
-    } else {
-        this.inMemoryDb.delete(key);
-        return null;
+    const entry = this.inMemoryDb.get(key);
+    if (!entry) {
+      return null;
     }
+    if (new Date().getTime() > entry.expiry) {
+      this.inMemoryDb.delete(key);
+      return null;
+    }
+    return entry.value;
   }
 
-  evict(type: string, args: string[]): void {
-    const key = `${type} ${JSON.stringify(args)}`;
-    this.inMemoryDb.delete(key);
-  }
+  evict() {
 
-  clearAll(): void {
-    this.inMemoryDb.clear();
   }
 }
