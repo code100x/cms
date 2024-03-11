463d834d0c98b8d3c5490b1fc126282405aa758f
tanmay4l <89733575+TanmayDhobale@users.noreply.github.com>
2024-03-11 20:09:29 +0530

    Cache utility with dynamic entry expiry

--- src/db/Cache.ts
+++ src/db/Cache.ts
@@ -1,50 +1,43 @@
 export class Cache {
-  private inMemoryDb: Map<
-    string,
-    {
-      value: any;
-      expiry: number;
-    }
-  >;
+  private inMemoryDb: Map<string, { value: any; expiry: number; }>;
   private static instance: Cache;
 
   private constructor() {
-    this.inMemoryDb = new Map<
-      string,
-      {
-        value: any;
-        expiry: number;
+    this.inMemoryDb = new Map();
+  }
+
+  static getInstance(): Cache {
+    if (!Cache.instance) {
+      Cache.instance = new Cache();
+    }
+    return Cache.instance;
+  }
+
+  set(type: string, args: string[], value: any, expiryInSeconds: number = 3600): void {
+    const currentTime = new Date().getTime();
+    const expiry = currentTime + expiryInSeconds * 1000;
+    this.inMemoryDb.set(`${type} ${JSON.stringify(args)}`, { value, expiry });
+  }
+
+  get(type: string, args: string[]): any | null {
+    const key = `${type} ${JSON.stringify(args)}`;
+    const cacheEntry = this.inMemoryDb.get(key);
+    if (cacheEntry) {
+      if (cacheEntry.expiry > new Date().getTime()) {
+        return cacheEntry.value;
+      } else {
+        this.inMemoryDb.delete(key);
       }
-    >();
-  }
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = new Cache();
     }
-
-    return this.instance;
+    return null;
   }
 
-  set(type: string, args: string[], value: any) {
-    this.inMemoryDb.set(`${type} ${JSON.stringify(args)}`, {
-      value,
-      expiry:
-        new Date().getTime() +
-        parseInt(process.env.CACHE_EXPIRE_S || '100', 10) * 1000,
-    });
+  evict(type: string, args: string[]): void {
+    const key = `${type} ${JSON.stringify(args)}`;
+    this.inMemoryDb.delete(key);
   }
 
-  get(type: string, args: string[]) {
-    const value = this.inMemoryDb.get(`${type} ${JSON.stringify(args)}`);
-    if (!value) {
-      return null;
-    }
-    if (new Date().getTime() > value.expiry) {
-      this.inMemoryDb.delete(`${type} ${JSON.stringify(args)}`);
-      return null;
-    }
-    return value.value;
+  clearAll(): void {
+    this.inMemoryDb.clear();
   }
-
-  evict() {}
 }
