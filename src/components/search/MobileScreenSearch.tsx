a84a75476ec4c2e14d3aa1044ab6ca881b1c2ba3
Nimit Haria <nimitharia007@gmail.com>
2024-03-27 12:56:39 +0530

    feat: added video search

---
+++ src/components/search/MobileScreenSearch.tsx
@@ -0,0 +1,25 @@
+import { SearchIcon, XCircleIcon } from 'lucide-react';
+import React, { useState } from 'react';
+import SearchBar from './SearchBar';
+
+const MobileScreenSearch = () => {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const toggleSearchBar = () => {
+    setShowSearchBar((prev) => !prev);
+  };
+  return (
+    <div className="md:hidden">
+      {showSearchBar ? (
+        <div className="absolute top-0 px-3 left-0 h-16 bg-white dark:bg-[#020817] border-b z-[100] w-full flex items-center gap-3">
+          <SearchBar />
+          <XCircleIcon onClick={toggleSearchBar} />
+        </div>
+      ) : (
+        <SearchIcon onClick={toggleSearchBar} size={18} />
+      )}
+    </div>
+  );
+};
+
+export default MobileScreenSearch;
---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,21 @@
+import { RefObject, useEffect } from 'react';
+
+const useClickOutside = (
+  ref: RefObject<HTMLElement>,
+  handler: (event: MouseEvent) => void,
+) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler(event);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, handler]);
+};
+
+export default useClickOutside;
