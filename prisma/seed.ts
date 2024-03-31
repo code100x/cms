00b842a1a0d3b3eaf662ff2296c650f8996f225e
devsargam <sargampoudel100@gmail.com>
2024-03-31 21:36:06 +0530

    fix(prisma): improve the seed command

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,38 @@
 import db from '../src/db/index';
 
 async function main() {
-  await db.user.createMany({
-    data: [
-      {
-        id: '2',
-        email: 'testuser',
-        name: 'Test User 1',
-        disableDrm: false,
-      },
-      {
-        id: '1',
-        email: 'testuser2',
-        name: 'Test User 2',
-        disableDrm: false,
-      },
-    ],
+  await db.user.upsert({
+    where: {
+      id: '2',
+      email: 'testuser',
+    },
+    create: {
+      id: '2',
+      email: 'testuser',
+      name: 'Test User 1',
+      disableDrm: false,
+    },
+    update: {},
   });
+
+  await db.user.upsert({
+    where: {
+      id: '1',
+      email: 'testuser2',
+    },
+    create: {
+      id: '1',
+      email: 'testuser2',
+      name: 'Test User 2',
+      disableDrm: false,
+    },
+    update: {},
+  });
+
+  if (await db.course.findMany()) {
+    console.error('DB is already seeded!');
+    process.exit(0);
+  }
 
   await db.course.createMany({
     data: [
