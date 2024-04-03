21a7f13d012b35f67ff6b9eb98562a2f8357d4fd
mvking5464 <mvking5464@gmail.com>
2024-04-04 01:39:39 +0530

    Added offline alert

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,15 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@/components/theme-provider';
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
+import { toast } from 'sonner';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    window.addEventListener('offline', () => toast.loading('You are offline'));
+    window.addEventListener('online', () => toast.dismiss());
+  }, []);
   return (
     <SessionProvider>
       <ThemeProvider
