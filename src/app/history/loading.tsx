8aa26af59a89df42515e647ac65113b5628d6bbc
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2024-04-12 10:41:02 +0530

    fixed Watch history skeletons are bigger than needed

--- src/app/history/loading.tsx
+++ src/app/history/loading.tsx
@@ -1,11 +1,32 @@
-import { CourseSkeleton } from '@/components/CourseCard';
+import { Fragment } from 'react';
 
 export default function Loading() {
   return (
-    <div className="max-w-screen-xl justify-between mx-auto p-4 cursor-pointer grid grid-cols-1 gap-5 md:grid-cols-3">
-      {[1, 2, 3].map((v) => (
-        <CourseSkeleton key={v} />
-      ))}
+    <div className="px-5 md:px-20 py-4">
+      <div className="rounded bg-slate-50 dark:bg-slate-900 h-8 w-40"></div>
+      <div className="flex flex-col gap-4 my-4 sm:my-8 max-w-full">
+        {[1, 2].map((_, index) => {
+          return (
+            <Fragment key={index}>
+              <div className="rounded bg-slate-50 dark:bg-slate-900 h-6 w-20"></div>
+              <div className="flex items-center gap-6">
+                {[1, 2, 3, 4].map((_, index) => (
+                  <div>
+                    <div
+                      className="w-[260px] h-36 bg-slate-50 dark:bg-slate-900 rounded-md"
+                      key={index}
+                    />
+                    <div className="flex flex-col gap-2 mt-4">
+                      <div className="w-24 h-4 bg-slate-50 dark:bg-slate-900 rounded-md"></div>
+                      <div className="w-16 h-4 bg-slate-50 dark:bg-slate-900 rounded-md"></div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 }
