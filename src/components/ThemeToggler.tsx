61092be6cf715154db7d5b35106c6db1cb54c188
Shivam Shahi <shivam.shahi06@gmail.com>
2024-04-12 21:53:33 +0530

    feat:changed theme dropdown to toggle

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,39 +1,61 @@
 'use client';
 
 import * as React from 'react';
-import { SunDimIcon, SunMoonIcon } from 'lucide-react';
+import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import { Button } from './ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Button } from '@/components/ui/button';
 
 export function ThemeToggler() {
-  const { setTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  function onThemeChange() {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+  }
 
   return (
-    <DropdownMenu modal={false}>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="iconSM">
-          <SunDimIcon className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <SunMoonIcon className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+    <div className="">
+      <div className="flex items-center justify-center">
+        <Button
+          aria-label="Switch theme"
+          variant="outline"
+          onClick={onThemeChange}
+          size="iconSM"
+          className="flex flex-col items-center border  justify-center w-10 h-10   ml-1 overflow-hidden font-medium duration-200 ease-in-out rounded-xl sm:p-4 text-text hover:bg-overlay"
+        >
+          <AnimatePresence mode="wait">
+            {resolvedTheme === 'light' && (
+              <motion.span
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                key="theme1"
+              >
+                <Moon className="h-[1.2rem] w-[1.2rem]" />
+              </motion.span>
+            )}
+            {resolvedTheme === 'dark' && (
+              <motion.span
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                key="theme2"
+              >
+                <Sun className="h-[1.2rem] w-[1.2rem]" />
+              </motion.span>
+            )}
+          </AnimatePresence>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className=" z-[200]">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </div>
+    </div>
   );
 }
