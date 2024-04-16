8a4b86e9fa060b99e974766c0e6dbc67f99d5496
Vijay <74645268+vijaysingh2219@users.noreply.github.com>
2024-04-17 00:05:43 +0530

    Integrated Youtube Renderer with video player

--- src/components/YoutubeRenderer.tsx
+++ src/components/YoutubeRenderer.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 
 interface Props {
   videoURL: string;
-  width: string;
-  height: string;
+  width?: string;
+  height?: string;
   style?: React.CSSProperties;
+  className?: string;
 }
 
-export const YoutubeRenderer = ({ width, height, videoURL, style }: Props) => {
+export const YoutubeRenderer = ({
+  width,
+  height,
+  videoURL,
+  style,
+  className,
+}: Props) => {
   if (!videoURL) {
     return null;
   }
   return (
     <iframe
-      width={width}
-      height={height}
+      width={width || 1280}
+      height={height || 720}
       src={videoURL}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerPolicy="strict-origin-when-cross-origin"
       allowFullScreen
       style={style}
+      className={className || ''}
     ></iframe>
   );
 };
--- src/components/admin/ContentRendererClient.tsx
+++ src/components/admin/ContentRendererClient.tsx
@@ -5,6 +5,20 @@ import { VideoPlayerSegment } from '@/components/VideoPlayerSegment';
 import VideoContentChapters from '../VideoContentChapters';
 import { useMemo, useState } from 'react';
 import { handleMarkAsCompleted } from '@/lib/utils';
+import YoutubeRenderer from '../YoutubeRenderer';
+
+const isYouTubeUrl = (url: string): boolean => {
+  const youtubePattern: RegExp =
+    /(?:youtu|youtube)(?:\.com|\.be)\/(?:watch\?v=)?([\w-]+)/;
+  return youtubePattern.test(url);
+};
+
+const extractYouTubeIdFromUrl = (url: string): string | null => {
+  const match: RegExpMatchArray | null = url.match(
+    /(?:youtu\.be\/|(?:youtube\.com\/(?:v\/|u\/\w\/|embed\/|watch\?v=)))([\w-]+)/,
+  );
+  return match ? match[1] : null;
+};
 
 export const ContentRendererClient = ({
   metadata,
@@ -63,6 +77,11 @@ export const ContentRendererClient = ({
         src: mpdUrl,
         type: 'application/x-mpegURL',
       };
+    } else if (isYouTubeUrl(mpdUrl)) {
+      return {
+        src: mpdUrl,
+        type: 'video/youtube',
+      };
     }
     return {
       src: mpdUrl,
@@ -89,101 +108,107 @@ export const ContentRendererClient = ({
 
   return (
     <div className="flex gap-2 items-start flex-col lg:flex-row">
-      <div className="flex-1 w-full">
-        <VideoPlayerSegment
-          setQuality={setQuality}
-          contentId={content.id}
-          subtitles={metadata.subtitles}
-          thumbnails={[]}
-          segments={metadata?.segments || []}
-          videoJsOptions={{
-            playbackrates: [0.5, 1, 1.25, 1.5, 1.75, 2],
-            controls: true,
-            fluid: true,
-            html5: {
-              vhs: {
-                overridenative: true,
-              },
-            },
-            thumbnail: metadata.thumbnail || false, // data.isComposite ? data.thumbnails[0] : null,
-            isComposite: true,
-            height: 720,
-            width: 1080,
-            delta: 30,
-            autoplay: true,
-            responsive: true,
-            sources: [source],
-          }}
-          onVideoEnd={() => {
-            setContentCompleted(true);
-          }}
+      {source.type === 'video/youtube' ? (
+        <YoutubeRenderer
+          width="1200"
+          videoURL={`https://www.youtube.com/embed/${extractYouTubeIdFromUrl(source.src)}`}
         />
-        <br />
-        <div className="flex justify-between mb-2">
-          <div>
-            <div className="text-gray-900 dark:text-white font-bold text-2xl">
-              {content.title}
+      ) : (
+        <div className="flex-1 w-full">
+          <VideoPlayerSegment
+            setQuality={setQuality}
+            contentId={content.id}
+            subtitles={metadata.subtitles}
+            thumbnails={[]}
+            segments={metadata?.segments || []}
+            videoJsOptions={{
+              playbackrates: [0.5, 1, 1.25, 1.5, 1.75, 2],
+              controls: true,
+              fluid: true,
+              html5: {
+                vhs: {
+                  overridenative: true,
+                },
+              },
+              thumbnail: metadata.thumbnail || false, // data.isComposite ? data.thumbnails[0] : null,
+              isComposite: true,
+              height: 720,
+              width: 1080,
+              delta: 30,
+              autoplay: true,
+              responsive: true,
+              sources: [source],
+            }}
+            onVideoEnd={() => {
+              setContentCompleted(true);
+            }}
+          />
+          <br />
+          <div className="flex justify-between mb-2">
+            <div>
+              <div className="text-gray-900 dark:text-white font-bold text-2xl">
+                {content.title}
+              </div>
+
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 my-4"
+                disabled={loadingMarkAs}
+                onClick={handleMarkCompleted}
+              >
+                {contentCompleted ? 'Mark as Incomplete' : 'Mark as completed'}
+              </button>
             </div>
 
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 my-4"
-              disabled={loadingMarkAs}
-              onClick={handleMarkCompleted}
-            >
-              {contentCompleted ? 'Mark as Incomplete' : 'Mark as completed'}
-            </button>
+            <div>
+              {/* <QualitySelector /> */}
+              <br />
+              {metadata.slides ? (
+                <div
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row-reverse',
+                    gap: '10px',
+                  }}
+                >
+                  <a href={metadata.slides} target="_blank">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2">
+                      Slides
+                    </button>
+                  </a>
+                </div>
+              ) : null}
+              {!showChapters && metadata.segments?.length > 0 && (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2"
+                  onClick={() => {
+                    scrollTo({ top: 0, behavior: 'smooth' });
+                    toggleShowChapters();
+                  }}
+                >
+                  View All Chapters
+                </button>
+              )}
+            </div>
           </div>
-
-          <div>
-            {/* <QualitySelector /> */}
-            <br />
-            {metadata.slides ? (
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row-reverse',
-                  gap: '10px',
-                }}
-              >
-                <a href={metadata.slides} target="_blank">
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2">
-                    Slides
-                  </button>
-                </a>
-              </div>
-            ) : null}
-            {!showChapters && metadata.segments?.length > 0 && (
+          {nextContent ? (
+            <div className="flex flex-row-reverse">
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
                 onClick={() => {
-                  scrollTo({ top: 0, behavior: 'smooth' });
-                  toggleShowChapters();
+                  const originalPath = window.location.pathname;
+                  const parts = originalPath.split('/');
+                  parts.pop();
+                  parts.push(nextContent.id.toString());
+                  const newPath = parts.join('/');
+                  router.push(newPath);
                 }}
               >
-                View All Chapters
-              </button>
-            )}
-          </div>
+                {nextContent.title}
+              </button>{' '}
+            </div>
+          ) : null}
         </div>
-        {nextContent ? (
-          <div className="flex flex-row-reverse">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4"
-              onClick={() => {
-                const originalPath = window.location.pathname;
-                const parts = originalPath.split('/');
-                parts.pop();
-                parts.push(nextContent.id.toString());
-                const newPath = parts.join('/');
-                router.push(newPath);
-              }}
-            >
-              {nextContent.title}
-            </button>{' '}
-          </div>
-        ) : null}
-      </div>
-
+      )}
       {showChapters && (
         <VideoContentChapters
           segments={metadata?.segments}
