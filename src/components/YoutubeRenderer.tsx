cd5a11498dc4b0ae24132234aca7ffae635b56cc
VK-RED <vishnukumart96@gmail.com>
2024-04-18 19:17:55 +0530

    feat: add Youtube Renderer

---
+++ src/components/YoutubeRenderer.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from 'react';
+
+interface YoutubeRendererProps {
+  url: string;
+}
+
+export const YoutubeRenderer: FunctionComponent<YoutubeRendererProps> = ({
+  url,
+}) => {
+  return (
+    <div className="mt-2 flex justify-center">
+      <iframe
+        width="100%"
+        className="h-[80vh]"
+        height="500px"
+        src={url}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
