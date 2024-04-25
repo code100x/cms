1ec005140ebcbbaafd8b1484a65256008e0c70c3
TANMAY <tanmay@TANMAYs-MacBook-Air.local>
2024-04-25 22:33:33 +0530

    fixed uniqu key for each student

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import crypto from 'crypto';
 
 interface CourseData {
   id: string;
@@ -26,6 +27,10 @@ const CertificateComponent = ({
     ? formatDate(course.date)
     : formatDate(new Date());
 
+  // Generate a unique ID for the student
+  const uniqueId =
+    `${crypto.randomBytes(8).toString('hex')  }-${  userName.replace(/\s/g, '')}`;
+
   return (
     <div className="card mx-auto w-3/5 border border-gray-300 shadow-lg mt-12 mb-6 relative">
       <div id="certificateArea" className="relative p-6">
@@ -39,16 +44,13 @@ const CertificateComponent = ({
           className="absolute top-1/4 left-1/2 transform -translate-x-1/2 text-center text-black"
         >
           <h2 className="text-2xl font-bold">{userName}</h2>
-
           <p className="text-lg font-bold">
-            Certificate ID: {course.id.toString().toUpperCase()}
+            Certificate ID: {uniqueId.toString().toUpperCase()}
           </p>
-
           <p className="absolute -bottom-11 w-full text-center text-base">
             Verified at: 100x@gmail.com
           </p>
           <h3 className="text-xl font-bold">{course.title}</h3>
-
           {/* <img src="/sign.png" alt="Signature" className="mx-auto h-16 mb-5 mr-24" /> */}
           <p className="text-base mt-36">Date of Issue: {currentDate}</p>
         </div>
