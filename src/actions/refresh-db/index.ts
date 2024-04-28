be1b267d6915f00e5445e7ade522f20b73ea9380
Krish <prathamrajsri2003@gmail.com>
2024-04-28 14:36:52 +0530

    fixed refresh-db

--- src/actions/refresh-db/index.ts
+++ src/actions/refresh-db/index.ts
@@ -53,7 +53,7 @@ export const refreshDb: RefreshDbFn = async () => {
       const courseId = course.appxCourseId.toString();
       const data = await checkUserEmailForPurchase(email, courseId);
 
-      if (data) {
+      if (data.data === '1') {
         responses.push(course);
       }
     });
