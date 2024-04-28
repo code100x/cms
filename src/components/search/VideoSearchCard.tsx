c6f51e387da8e626a3817b267badb3455544fa5b
Zaheer Abbas <contact@zaheerabbas.dev>
2024-04-28 21:02:29 +0530

    Show course number for multiple courses in search results for a video

--- src/components/search/VideoSearchCard.tsx
+++ src/components/search/VideoSearchCard.tsx
@@ -1,31 +1,34 @@
-import { TSearchedVideos } from '@/app/api/search/route';
 import { PlayCircleIcon } from 'lucide-react';
 import React from 'react';
 
 const VideoSearchCard = ({
-  video,
+  videoId,
+  parentId,
+  courseId,
+  videoTitle,
   onCardClick,
 }: {
-  video: TSearchedVideos;
+  videoId: number;
+  parentId: number;
+  courseId: number;
+  videoTitle: string;
   onCardClick: (videoUrl: string) => void;
 }) => {
-  const { id: videoId, parentId, parent } = video;
-
-  if (parentId && parent) {
-    const courseId = parent.courses[0].courseId;
-    const videoUrl = `/courses/${courseId}/${parentId}/${videoId}`;
-    return (
-      <div
-        className="px-3 py-2 hover:bg-gray-200 hover:dark:bg-gray-700 cursor-pointer flex items-center gap-3"
-        onClick={() => onCardClick(videoUrl)}
-      >
-        <div className="min-w-content">
-          <PlayCircleIcon className="w-4 h-4" />
-        </div>
-        <span className="w-4/5">{video.title}</span>
+  const videoUrl = `/courses/${courseId}/${parentId}/${videoId}`;
+  return (
+    <div
+      className="px-3 py-2 hover:bg-gray-200 hover:dark:bg-gray-700 cursor-pointer flex items-center gap-3"
+      onClick={() => onCardClick(videoUrl)}
+    >
+      <div className="min-w-content">
+        <PlayCircleIcon className="w-4 h-4" />
       </div>
-    );
-  }
+      {/* // NOTE: Ideally course name should be showed instead of course Id(?) */}
+      <span className="w-4/5">
+        Course: {courseId} - {videoTitle}
+      </span>
+    </div>
+  );
 };
 
 export default VideoSearchCard;
