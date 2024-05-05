23b5255a819c77d08dad6784873cb840e1f3bad8
iamnithishraja <153272518+iamnithishraja@users.noreply.github.com>
2024-05-05 19:05:23 +0530

    fixed docker hot reaload on windows

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     serverActions: {
       allowedOrigins: ['localhost:3000', 'app.100xdevs.com', 'app2.100xdevs.com']
     }
-  }
-};
+  },
+  swcMinify: true,
+  webpack: (config, context) => {
+    // Enable polling based on env variable being set
+    if(process.env.NEXT_WEBPACK_USEPOLLING) {
+      config.watchOptions = {
+        poll: 500,
+        aggregateTimeout: 300
+      }
+    }
+    return config
+  },
+}
+
+
 
 module.exports = nextConfig;
