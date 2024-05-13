7f18d93ee7d0f9c3af94d72e9d6ad0424587dcf1
hkirat <harkirat96@gmail.com>
2024-05-13 10:36:21 +0530

    revert video player for now

--- src/components/videothumbnail.tsx
+++ src/components/videothumbnail.tsx
@@ -2,44 +2,53 @@ import React, { useState } from 'react';
 import VideoPreview from '@/actions/videopreview/videoPreview';
 import { useEffect } from 'react';
 
-const VideoThumbnail = ({ imageUrl, contentId }: { imageUrl: string , contentId: number }) => {
-    const [videoUrl, setVideoUrl] = useState<string | null>(null);
-    const [hover,setHover] = useState(false);
+const VideoThumbnail = ({
+  imageUrl,
+  contentId,
+}: {
+  imageUrl: string;
+  contentId: number;
+}) => {
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const [hover, setHover] = useState(false);
 
-    useEffect(() => {
-        async function fetchVideoUrl() {
-            const url = await VideoPreview({ contentId });
-            setVideoUrl(url);
-        }
-        fetchVideoUrl();
-    }, [contentId]);
-    const handleMouseEnter = () => {
-        setHover(true);
-    };
+  useEffect(() => {
+    async function fetchVideoUrl() {
+      const url = await VideoPreview({ contentId });
+      setVideoUrl(url);
+    }
+    fetchVideoUrl();
+  }, [contentId]);
+  const handleMouseEnter = () => {
+    setHover(true);
+  };
 
-    const handleMouseLeave = () => {
-        setHover(false);
-    };
-    return (
-        <div className="m max-h-[573px] max-w-[1053px]  relative" onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}>
-            <div className=" w-full h-full relative">
-                {hover && videoUrl ? (
-                    <div className='w-full h-full'>
-                    <video muted autoPlay width="100%" height="100%">
-                        <source src={videoUrl} type="video/mp4"/>
-                    </video>
-                    </div>
-                ) : (
-                    <img
-                        src={imageUrl}
-                        alt="Video Thumbnail"
-                        className=" w-full h-full object-cover"
-                    />
-                )}
-            </div>
-        </div>
-    );
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+  return (
+    <div
+      className="m max-h-[573px] max-w-[1053px]  relative"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className=" w-full h-full relative">
+        {hover && videoUrl ? (
+          <div className="w-full h-full">
+            <video muted autoPlay width="100%" height="100%">
+              <source src={videoUrl} type="video/mp4" />
+            </video>
+          </div>
+        ) : (
+          <img
+            src={imageUrl}
+            alt="Video Thumbnail"
+            className=" w-full h-full object-cover"
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default VideoThumbnail;
