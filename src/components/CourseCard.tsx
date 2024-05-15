e96d1bb3240e82640d1786e421f63ec32a1c4a7f
unknown <sanusingh1603@gmail.com>
2024-05-15 17:57:32 +0530

    Minor fixes

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -1,6 +1,11 @@
 'use client';
 import { Course } from '@/store/atoms';
 import PercentageComplete from './PercentageComplete';
+import { ChevronRight } from 'lucide-react';
+import router from 'next/router';
+import { PrimaryButton } from './buttons/PrimaryButton';
+import { SecondaryButton } from './buttons/SecondaryButton';
+import { Button } from './ui/button';
 // import { PrimaryButton } from './buttons/PrimaryButton';
 // import { SecondaryButton } from './buttons/SecondaryButton';
 // import { useRouter } from 'next/navigation';
@@ -55,56 +60,56 @@ export const CourseCard = ({
       </div>
     </div>
   );
-  // return (
-  //   <div
-  //     className={`max-w-full flex flex-col md:flex-row items-center bg-slate-100 md:border rounded-${roundedCardSize} shadow-lg dark:bg-gradient-to-t md:dark:bg-gradient-to-l dark:from-slate-900 dark:to-slate-800`}
-  //     onClick={() => {
-  //       onClick();
-  //     }}
-  //   >
-  //     <div className="relative">
-  //       <div className="p-2">
-  //         <img
-  //           src={course.imageUrl}
-  //           alt={course.title}
-  //           className="rounded-t-md md:rounded-l-md md:rounded-r-none md:max-w-md"
-  //         />
-  //       </div>
-  //     </div>
+  return (
+    <div
+      className={`max-w-full flex flex-col md:flex-row items-center bg-slate-100 md:border rounded-${roundedCardSize} shadow-lg dark:bg-gradient-to-t md:dark:bg-gradient-to-l dark:from-slate-900 dark:to-slate-800`}
+      // onClick={() => {
+      //   onClick();
+      // }}
+    >
+      <div className="relative">
+        <div className="p-2">
+          <img
+            src={course.imageUrl}
+            alt={course.title}
+            className="rounded-t-md md:rounded-l-md md:rounded-r-none md:max-w-md"
+          />
+        </div>
+      </div>
 
-  //     <div className="px-6 py-2 w-full h-full">
-  //       <div className="flex flex-col w-full h-full items-start justify-between md:py-4">
-  //         <div className="w-full items-start mb-3">
-  //           <h2 className="mb-0 md:mb-2 dark:text-neutral-100 text-neutral-800 text-xl sm:text-3xl font-semibold">
-  //             {course.title} Cohort
-  //           </h2>
+      <div className="px-6 py-2 w-full h-full">
+        <div className="flex flex-col w-full h-full items-start justify-between md:py-4">
+          <div className="w-full items-start mb-3">
+            <h2 className="mb-0 md:mb-2 dark:text-neutral-100 text-neutral-800 text-xl sm:text-3xl font-semibold">
+              {course.title} Cohort
+            </h2>
 
-  //           <p className="dark:text-neutral-200 text-neutral-700 font-medium">
-  //             {course.description}
-  //           </p>
-  //         </div>
+            <p className="dark:text-neutral-200 text-neutral-700 font-medium">
+              {course.description}
+            </p>
+          </div>
 
-  //         <div className="w-full flex justify-end pb-2 md:pb-0">
-  //           <Button className="group">
-  //             Explore Content{' '}
-  //             <ChevronRight className="h-4 w-4 ml-1 group-hover:translate-x-1 transition" />
-  //           </Button>
-  //         </div>
-  //         <PrimaryButton>View Content</PrimaryButton>
-  //         {course.certIssued && (
-  //           <SecondaryButton
-  //             onClick={(e) => {
-  //               e.stopPropagation();
-  //               router.push('/certificate');
-  //             }}
-  //           >
-  //             Download Certificate
-  //           </SecondaryButton>
-  //         )}
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+          <div className="w-full flex justify-end pb-2 md:pb-0">
+            <Button className="group">
+              Explore Content{' '}
+              <ChevronRight className="h-4 w-4 ml-1 group-hover:translate-x-1 transition" />
+            </Button>
+          </div>
+          <PrimaryButton>View Content</PrimaryButton>
+          {course.certIssued && (
+            <SecondaryButton
+              onClick={(e) => {
+                e.stopPropagation();
+                router.push('/certificate');
+              }}
+            >
+              Download Certificate
+            </SecondaryButton>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const CourseSkeleton = () => {
