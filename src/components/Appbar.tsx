a3c729db785f2af8a057840eb512ea6a11df32c7
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-05-16 19:56:43 +0530

    fix/632 appbar fixes, hide join now and login button while loading

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -17,9 +17,11 @@ import ProfileDropdown from './profile-menu/ProfileDropdown';
 import { ThemeToggler } from './ThemeToggler';
 
 export const Appbar = () => {
-  const session = useSession();
+  const { data: session, status: sessionStatus } = useSession();
   const [sidebarOpen, setSidebarOpen] = useRecoilState(sidebarOpenAtom);
   const currentPath = usePathname();
+
+  const isLoading = sessionStatus === 'loading';
 
   return (
     <>
@@ -35,38 +37,40 @@ export const Appbar = () => {
         <div className="md:max-w-screen-2xl mx-auto flex items-center justify-between w-full">
           <Logo onFooter={false} />
 
-          {session?.data?.user ? (
-            <>
-              <div className="hidden md:block">
-                <SearchBar />
-              </div>
-              <div className="flex items-center space-x-2">
-                {/* Search Bar for smaller devices */}
-                <MobileScreenSearch />
-                <ProfileDropdown />
-              </div>
-            </>
-          ) : (
-            <div className="flex items-center space-x-2">
-              <div className="hidden sm:flex items-center justify-around md:w-auto md:block space-x-3">
-                <AppbarAuth />
+          {session?.user
+            ? !isLoading && (
+                <>
+                  <div className="hidden md:block">
+                    <SearchBar />
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    {/* Search Bar for smaller devices */}
+                    <MobileScreenSearch />
+                    <ProfileDropdown />
+                  </div>
+                </>
+              )
+            : !isLoading && (
+                <div className="flex items-center space-x-2">
+                  <div className="hidden sm:flex items-center justify-around md:w-auto md:block space-x-3">
+                    <AppbarAuth />
 
-                <Button size={'sm'} asChild>
-                  <Link
-                    href={'https://harkirat.classx.co.in/new-courses'}
-                    target="_blank"
-                  >
-                    <p className="text-white">Join now</p>{' '}
-                    <Sparkles className="text-white ml-2 h-4 w-4 hover:translate-x-0.5 ease-linear duration-200" />
-                  </Link>
-                </Button>
-              </div>
-              <ThemeToggler />
-              <div className="block sm:hidden">
-                <NavigationMenu />
-              </div>
-            </div>
-          )}
+                    <Button size={'sm'} asChild>
+                      <Link
+                        href={'https://harkirat.classx.co.in/new-courses'}
+                        target="_blank"
+                      >
+                        <p className="text-white">Join now</p>{' '}
+                        <Sparkles className="text-white ml-2 h-4 w-4 hover:translate-x-0.5 ease-linear duration-200" />
+                      </Link>
+                    </Button>
+                  </div>
+                  <ThemeToggler />
+                  <div className="block sm:hidden">
+                    <NavigationMenu />
+                  </div>
+                </div>
+              )}
         </div>
       </nav>
       <div className="h-16 w-full print:hidden" />
