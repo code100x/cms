9785b093c7d4b9b13ca2b72ad036ebbac9955b5c
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2024-05-27 12:22:54 +0530

    fixed env variable loading issue

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,12 +1,17 @@
+# docker compose up -d
+DIR="$(cd "$(dirname "$0")" && pwd)"
+export $(grep -v '^#' .env.test | xargs)
  docker run -d \
        --name db \
        -e POSTGRES_USER=postgres \
        -e POSTGRES_PASSWORD=postgres \
-       -e POSTGRES_DB=mydatabase \
+       -e POSTGRES_DB=cms \
        -p 5432:5432 \
        postgres
 echo '🟡 - Waiting for database to be ready...'
-/Users/piyush/cms/scripts/wait-for-it.sh  "postgresql://postgres" -- echo '🟢 - Database is ready!'
-npx prisma migrate dev --name init --schema=./prisma/schema.prisma
+$DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
+echo $DATABASE_URL
+npx prisma migrate dev --name init
 vitest -c ./vitest.config.integration.ts
 docker stop db && docker rm db
+# docker compose down
