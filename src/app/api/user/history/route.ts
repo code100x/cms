c0fa1bb6e328575cf0405e10ee12d7873ccf9b3e
thrishank <thrishankkalluru@gmail.com>
2024-05-30 02:49:16 +0530

    feature: added sorting of folders

---
+++ src/app/api/user/history/route.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+import db from '@/db';
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const userId = url.searchParams.get('id');
+
+  if (!userId) {
+    return NextResponse.json('Id not found');
+  }
+
+  const data = await db.videoProgress.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      content: {
+        include: {
+          parent: {
+            select: {
+              id: true,
+              courses: true,
+            },
+          },
+        },
+      },
+    },
+    orderBy: {
+      updatedAt: 'desc',
+    },
+  });
+  return NextResponse.json(data);
+}
--- src/components/FolderView.tsx
+++ src/components/FolderView.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from 'next/navigation';
 import { ContentCard } from './ContentCard';
 import { Bookmark } from '@prisma/client';
+import React, { useEffect, useState } from 'react';
+import { getSession } from 'next-auth/react';
 
 export const FolderView = ({
   courseContent,
@@ -37,11 +39,63 @@ export const FolderView = ({
   }
   // why? because we have to reset the segments or they will be visible always after a video
 
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortby(e.target.value);
+  };
+
+  const [recentVideo, setRecentvideo] = useState(1);
+  useEffect(() => {
+    async function getHistory() {
+      const session = await getSession();
+      if (!session?.user) {
+        return [];
+      }
+
+      const userId = session.user.id;
+      const res = await fetch(`/api/user/history?id=${userId}`);
+      const userHistory = await res.json();
+      setRecentvideo(userHistory[0].content.parent.id);
+    }
+    getHistory();
+  }, []);
+
+  const [sortby, setSortby] = useState('same');
+  const [sortedContent, setSortedContent] = useState(courseContent);
+  useEffect(() => {
+    let sorted = [...courseContent];
+    if (sortby === 'reverse') {
+      sorted = sorted.slice().reverse();
+    }
+    if (sortby === 'recent') {
+      const start = [];
+      let i = 0;
+      while (i < sorted.length && recentVideo !== sorted[i].id) {
+        start.push(sorted[i]);
+        i++;
+      }
+      sorted = sorted.slice(i).concat(start);
+    }
+    setSortedContent(sorted);
+  }, [sortby]);
+
   return (
     <div>
       <div></div>
+
+      <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 mx-auto">
+        <select
+          value={sortby}
+          onChange={handleChange}
+          className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          <option value="reverse">Newest First</option>
+          <option value="same">Oldest First</option>
+          <option value="recent">Continue Watching</option>
+        </select>
+      </div>
+
       <div className="max-w-screen-xl justify-between mx-auto p-4 cursor-pointer grid grid-cols-1 gap-5 md:grid-cols-3">
-        {courseContent.map((content) => {
+        {sortedContent.map((content) => {
           const videoProgressPercent =
             content.type === 'video' &&
             content.videoFullDuration &&
