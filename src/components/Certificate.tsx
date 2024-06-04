cdb0e1b389956edd32fbec85525b4de5b55b8ee1
Nischal Shetty <nischal2004coc@gmail.com>
2024-06-04 23:53:48 +0530

    Fixed certificate download Bug

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -11,7 +11,9 @@ import { Button } from './ui/button';
 import { FaFileImage, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { useGenerateCertificate } from '@/hooks/useCertGen';
 import { OneCertificate } from '@/utiles/certificate';
-import { useMemo } from 'react'; //used to fix maximum update depth exceeded err
+import { useMemo, useState, useEffect } from 'react';
+
+type ActionType = 'downloadPNG' | 'shareLinkedIn' | 'shareTwitter';
 
 export const CertificateComponent = ({
   certificateId,
@@ -19,6 +21,10 @@ export const CertificateComponent = ({
   course,
   userName,
 }: OneCertificate & { userName: string }) => {
+  const [triggeredAction, setTriggeredAction] = useState<ActionType | null>(
+    null,
+  );
+
   const certificateDetails = useMemo(
     () => ({
       certificateId,
@@ -34,73 +40,80 @@ export const CertificateComponent = ({
     userName,
   });
 
-  // const handleDownloadPDF = async () => {
-  //   const downloadUrl = certificatePdfUrl;
-  //   const a = document.createElement('a');
-  //   a.href = downloadUrl!;
-  //   a.download = 'certificate.pdf';
-  //   document.body.appendChild(a);
-  //   a.click();
-  //   document.body.removeChild(a);
-  // };
+  useEffect(() => {
+    if (!certificateImageUrl || !triggeredAction) return;
 
-  const handleDownloadPNG = async () => {
-    const downloadUrl = certificateImageUrl;
-    console.log('downloadUrl is : ', downloadUrl);
-    const a = document.createElement('a');
-    a.href = downloadUrl;
-    a.download = 'certificate.png';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+    console.log('Url is : ', certificatePdfUrl);
+    switch (triggeredAction) {
+      case 'downloadPNG': {
+        const downloadUrl = certificateImageUrl;
+        const a = document.createElement('a');
+        a.href = downloadUrl;
+        a.download = 'certificate.png';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        break;
+      }
+      case 'shareLinkedIn': {
+        const certificateUrl = `${window.location.origin}/certificate/verify/${certificateSlug}`;
+        const postContent = `I just earned the "${course.title}" certificate on 100xDevs! Check it out: ${certificateUrl}`;
+        const linkedInShareUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
+          certificateUrl,
+        )}&text=${encodeURIComponent(postContent)}`;
+        window.open(linkedInShareUrl);
+        break;
+      }
+      case 'shareTwitter': {
+        const tweetText = `I just earned the "${course.title}" certificate on 100xDevs! Check it out:`;
+        const twitterShareUrl = `${window.location.origin}/certificate/verify/${certificateSlug}`;
+        const twitterShareLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}&url=${encodeURIComponent(twitterShareUrl)}`;
+        window.open(twitterShareLink);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+
+    setTriggeredAction(null);
+  }, [certificateImageUrl, triggeredAction, certificateSlug, course.title]);
+
+  const handleAction = (action: ActionType) => {
+    setTriggeredAction(action);
   };
-
-  const handleShareLinkedIn = async () => {
-    const certificateUrl = `${window.location.origin}/certificate/verify/${certificateSlug}`;
-    const postContent = `I just earned the "${course.title}" certificate on 100xDevs! Check it out: ${certificateUrl}`;
-    const shareUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
-      certificateUrl,
-    )}&text=${encodeURIComponent(postContent)}`;
-    window.open(shareUrl);
-  };
-
-  const handleShareTwitter = () => {
-    const tweetText = `I just earned the "${course.title}" certificate on 100xDevs! Check it out:`;
-    const certificateUrl = `${window.location.origin}/certificate/verify/${certificateSlug}`;
-    const shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}&url=${encodeURIComponent(certificateUrl)}`;
-    window.open(shareUrl);
-  };
-
-  console.log('Url is : ', certificatePdfUrl);
 
   return (
     <Card className="w-500 my-4" key={course.id}>
       <CardContent className="flex justify-center">
-        <img
-          src={certificateImageUrl}
-          alt=""
-          className="w-full max-w-screen-md"
-        />
+        {certificateImageUrl ? (
+          <img
+            src={certificateImageUrl}
+            alt="Certificate"
+            className="w-full max-w-screen-md"
+          />
+        ) : (
+          <div className="mt-5 text-2xl font-semibold leading-none tracking-tight">
+            Generating certificate...
+          </div>
+        )}
       </CardContent>
       <CardHeader>
         <CardTitle>{course.title}</CardTitle>
         <CardDescription>{course.description}</CardDescription>
       </CardHeader>
       <CardFooter className="flex justify-end">
-        {/* <Button onClick={() => handleDownloadPDF()} className="mr-2">
-          <FaDownload className="mr-1" /> Download PDF
-        </Button> */}
-        <Button onClick={() => handleDownloadPNG()} className="mr-2">
+        <Button onClick={() => handleAction('downloadPNG')} className="mr-2">
           <FaFileImage className="mr-1" /> Download PNG
         </Button>
         <div className="flex items-center justify-center">
           <Button
             className="share-button flex items-center mr-2 bg-transparent"
-            onClick={handleShareLinkedIn}
+            onClick={() => handleAction('shareLinkedIn')}
           >
             <FaLinkedin className="mr-1" /> Share on LinkedIn
           </Button>
-          <Button onClick={handleShareTwitter} className="mr-2">
+          <Button onClick={() => handleAction('shareTwitter')} className="mr-2">
             <FaTwitter className="mr-1" /> Share on Twitter
           </Button>
         </div>
