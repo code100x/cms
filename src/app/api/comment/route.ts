aae5615c7bb6674ef4ed70667ed07d3196a88f0e
Arpit Blagan <arpitblagan@Arpits-MacBook-Air.local>
2024-06-15 11:14:37 +0530

    added comment update service

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -2,15 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 import db from '../../../db';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../../../lib/auth';
-import { ROLES } from '../../../actions/types';
 import { revalidatePath } from 'next/cache';
 export const PATCH = async (req: NextRequest) => {
   const session = await getServerSession(authOptions);
 
-  if (!session || !session.user || session.user.role !== ROLES.ADMIN) {
-    return { error: 'Unauthorized or insufficient permissions' };
+  if (!session || !session.user) {
+    console.log(session);
+    return NextResponse.json({
+      error: 'Unauthorized or insufficient permissions',
+    });
   }
   const data = await req.json();
+  console.log('data', data);
+
   const comment = await db.comment.update({
     where: {
       id: data.commentId,
--- src/components/comment/CommentUpdateForm.tsx
+++ src/components/comment/CommentUpdateForm.tsx
@@ -17,9 +17,11 @@ import { toast } from 'sonner';
 const CommentUpdateForm = ({ commentId, comment }: any) => {
   const currentPath = usePathname();
   const [open, setOpen] = useState(false);
+  const [prevText, setPt] = useState('');
   const [text, setT] = useState('');
   useEffect(() => {
     setT(comment);
+    setPt(comment);
   }, []);
   const handleUpdate = async () => {
     const body = {
@@ -27,9 +29,13 @@ const CommentUpdateForm = ({ commentId, comment }: any) => {
       commentId,
       currentPath,
     };
+    if (text === prevText) {
+      return;
+    }
     try {
-      const res = await axios.post('/api/comment', body);
+      const res = await axios.patch('/api/comment', body);
       console.log(res);
+      setOpen(false);
       toast.success('successfully updated the comment');
     } catch (err) {
       console.log('update comment error', err);
@@ -37,55 +43,59 @@ const CommentUpdateForm = ({ commentId, comment }: any) => {
     }
   };
   return (
-    <div>
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger>
-          <div className="flex items-center gap-x-2">
-            <p>Update</p>
-            <Pencil className="h-4 w-4" />
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <div
+          className="flex items-center gap-x-2"
+          onClick={(e) => {
+            e.preventDefault();
+            setOpen(true);
+          }}
+        >
+          <p>Update</p>
+          <Pencil className="h-4 w-4" />
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Update Comment</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          <div className="mx-4 my-2 flex flex-col gap-4">
+            <div className="flex flex-col gap-4">
+              <label>Comment</label>
+              <Input
+                className="text-white"
+                value={text}
+                onChange={(e) => {
+                  setT(e.target.value);
+                }}
+              />
+            </div>
+            <div className="items-centert flex justify-between gap-3">
+              <Button
+                variant={'secondary'}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setOpen(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant={'default'}
+                onClick={(e) => {
+                  e.preventDefault();
+                  handleUpdate();
+                }}
+              >
+                Update
+              </Button>
+            </div>
           </div>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Update Comment</DialogTitle>
-          </DialogHeader>
-          <DialogDescription>
-            <form className="mx-4 my-2 flex flex-col gap-4">
-              <div className="flex flex-col gap-4">
-                <label>Comment</label>
-                <Input
-                  className="text-white"
-                  value={text}
-                  onChange={(e) => {
-                    setT(e.target.value);
-                  }}
-                />
-              </div>
-              <div className="items-centert flex justify-between gap-3">
-                <Button
-                  variant={'secondary'}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setOpen(false);
-                  }}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  variant={'default'}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleUpdate();
-                  }}
-                >
-                  Update
-                </Button>
-              </div>
-            </form>
-          </DialogDescription>
-        </DialogContent>
-      </Dialog>
-    </div>
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   );
 };
 
