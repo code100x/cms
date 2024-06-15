e870d841979e8691fbee8904fb3ca70c80795a68
Hitesh Sharma <hiteshsharma.2314@gmail.com>
2024-06-15 15:48:08 +0530

    minor fix

--- apps/next-frontend/src/actions/commentVote/index.ts
+++ apps/next-frontend/src/actions/commentVote/index.ts
@@ -1,20 +1,7 @@
 'use server';
 
 import { getServerSession } from 'next-auth';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> eabb399 (prettier and eslint fix, github templates)
-import {
-  InputTypeHandleVote,
-  ReturnTypeHandleVote,
-} from '@repo/common/zodTypes/commentVote';
-<<<<<<< HEAD
-=======
 import { InputTypeHandleVote, ReturnTypeHandleVote } from '@repo/common/zodTypes/commentVote';
->>>>>>> 1d267da (feat: apps/next-frontend packages/common packages/database packages/eslint packages/tsconfig packages/ui)
-=======
->>>>>>> eabb399 (prettier and eslint fix, github templates)
 import { authOptions } from '@repo/common/lib/auth';
 import prisma from '@repo/db';
 import { VoteType } from '@repo/db';
