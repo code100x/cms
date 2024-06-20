ceb98e4233233096fa7f78eff1128f38fbc4a7b1
hkirat <harkirat96@gmail.com>
2024-06-20 09:14:50 +0530

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,16 +8,21 @@ export const config = {
 export default withAuth(async (req) => {
   if (process.env.LOCAL_CMS_PROVIDER) return;
 
-  const token = req.nextauth.token;
-  if (!token) {
+  try {
+    const token = req.nextauth.token;
+    if (!token) {
+      return NextResponse.redirect(new URL('/invalidsession', req.url));
+    }
+    const user = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL_LOCAL}/api/user?token=${token.jwtToken}`,
+    );
+  
+    const json = await user.json();
+    if (!json.user) {
+      return NextResponse.redirect(new URL('/invalidsession', req.url));
+    }
+  } catch(e) {
     return NextResponse.redirect(new URL('/invalidsession', req.url));
   }
-  const user = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL_LOCAL}/api/user?token=${token.jwtToken}`,
-  );
 
-  const json = await user.json();
-  if (!json.user) {
-    return NextResponse.redirect(new URL('/invalidsession', req.url));
-  }
 });
