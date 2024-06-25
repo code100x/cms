984efe82cdda26f16654326e48458d928a249ffc
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-06-25 16:50:48 +0530

    Course form modified

--- src/app/api/admin/course/route.ts
+++ src/app/api/admin/course/route.ts
@@ -6,7 +6,10 @@ const requestBodySchema = z.object({
   adminSecret: z.string(),
   title: z.string(),
   description: z.string(),
-  imageUrl: z.string().url().or(z.string()),
+  imageUrl: z
+    .string()
+    .url({ message: 'URL should be valid image url' })
+    .or(z.string()),
   id: z.string(),
   slug: z.string(),
   appxCourseId: z.string(),
@@ -14,46 +17,57 @@ const requestBodySchema = z.object({
 });
 
 export async function POST(req: NextRequest) {
-  const parseResult = requestBodySchema.safeParse(await req.json());
+  const body = await req.json();
+  try {
+    const parseResult = requestBodySchema.safeParse(body);
 
-  if (!parseResult.success) {
+    if (!parseResult.success) {
+      return NextResponse.json(
+        { error: parseResult.error.message },
+        { status: 400 },
+      );
+    }
+
+    const {
+      adminSecret,
+      title,
+      description,
+      imageUrl,
+      id,
+      slug,
+      appxCourseId,
+      discordRoleId,
+    } = parseResult.data;
+
+    if (adminSecret !== process.env.ADMIN_SECRET) {
+      return NextResponse.json({}, { status: 401 });
+    }
+
+    await db.course.create({
+      data: {
+        title,
+        imageUrl,
+        slug,
+        description,
+        id: parseInt(id, 10),
+        appxCourseId: parseInt(appxCourseId, 10),
+        discordRoleId,
+      },
+    });
+
     return NextResponse.json(
-      { error: parseResult.error.message },
-      { status: 400 },
+      { message: 'Course Successfully added' },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      {
+        message: 'Internal Server error',
+      },
+      { status: 500 },
     );
   }
-
-  const {
-    adminSecret,
-    title,
-    description,
-    imageUrl,
-    id,
-    slug,
-    appxCourseId,
-    discordRoleId,
-  } = parseResult.data;
-
-  if (adminSecret !== process.env.ADMIN_SECRET) {
-    return NextResponse.json({}, { status: 401 });
-  }
-
-  await db.course.create({
-    data: {
-      title,
-      imageUrl,
-      slug,
-      description,
-      id: parseInt(id, 10),
-      appxCourseId: parseInt(appxCourseId, 10),
-      discordRoleId,
-    },
-  });
-
-  return NextResponse.json(
-    {},
-    {
-      status: 200,
-    },
-  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Providers } from './providers';
 import { GoogleAnalytics } from '@/components/analytics/GoogleAnalytics';
 import { siteConfig } from '@/config/site-config';
 import Footer from '@/components/landing/footer/footer';
-import { Toaster } from 'sonner';
 import NextTopLoader from 'nextjs-toploader';
+import { Toaster } from '@/components/ui/toaster';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -29,12 +29,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       >
         <GoogleAnalytics />
         <Providers>
+          <Toaster />
           <Appbar />
           <NextTopLoader color="#2E78C7" height={2} />
           {/* this is done as to keep footer in the bottom of the page */}
           <div className="min-h-[calc(100vh-64px)]">{children}</div>
           <Footer />
-          <Toaster richColors />
         </Providers>
       </body>
     </html>
