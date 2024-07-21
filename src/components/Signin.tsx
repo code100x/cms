6f8145e184e6f61d6a1d3344f7e7da83e332fade
Anurag12133 <anurajput62616@gmail.com>
2024-07-21 19:23:09 +0530

    loader-addede-on-signinbutton

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -153,7 +153,7 @@ const Signin = () => {
             onClick={handleSubmit}
           >
             {checkingPassword ? (
-              <ClipLoader size={20} color="#ffffff" />
+              <ClipLoader size={25} color="#ffffff" />
             ) : (
               'Login'
             )}
