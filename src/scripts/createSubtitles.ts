c3c7d27399bc4f5dbe8952808a1ae7c66f6bdb3b
Pritam12F <pritam.das.santu@gmail.com>
2024-08-14 12:40:18 +0000

    completed getting subtitles from whisperAI

--- src/scripts/createSubtitles.ts
+++ src/scripts/createSubtitles.ts
@@ -5,6 +5,12 @@ import {
   getMetadata,
   isUrlAccessible,
 } from '@/components/admin/ContentRenderer';
+import ffmpeg from 'fluent-ffmpeg';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const downloadVideo = (url: string, fileName: string) => {
   const temp = `${RootDir}/temp`;
@@ -14,10 +20,16 @@ export const downloadVideo = (url: string, fileName: string) => {
 
   const destination = `${temp}/${fileName}`;
   https.get(url, (resp) => resp.pipe(fs.createWriteStream(destination)));
+
+  return destination;
 };
 
-export const getVideoUrl = async (contentId: number): Promise<string> => {
+export const getVideoUrl = async (contentId: number): Promise<any> => {
   const metadata = await getMetadata(contentId);
+
+  if (!metadata) {
+    return null;
+  }
 
   if (await isUrlAccessible(metadata?.[360])) {
     return metadata?.[360];
@@ -28,8 +40,30 @@ export const getVideoUrl = async (contentId: number): Promise<string> => {
   return metadata?.[1080];
 };
 
-// export const convertVideoToMP3 = async (contentId: number) => {
-//   const url = await getVideoUrl(contentId);
-//   //TODO: check format of video here
-//   const downloadVideo = (url, 'example.');
-// };
+async function convertToSRT(destination: string) {
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream(destination),
+    model: 'whisper-1',
+  });
+
+  return transcription.text;
+}
+
+export const convertVideoToMP3 = async (contentId: number) => {
+  const url = await getVideoUrl(contentId);
+
+  if (!url) {
+    return;
+  }
+
+  const downloadedVideo = downloadVideo(url, `example.mp4`);
+
+  ffmpeg(downloadedVideo)
+    .output(`${RootDir}/temp/audio.mp3`)
+    .on('end', async () => {
+      console.log('Conversion finished');
+      await convertToSRT(`${RootDir}/temp/audio.mp3`);
+    })
+    .on('error', (err) => console.error('Error:', err))
+    .run();
+};
