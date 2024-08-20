7551f8a4d2a5a7128b57a7bf7095962727f289d4
hkirat <harkirat96@gmail.com>
2024-08-20 10:08:11 +0530

    Fixed admin dashboard

--- src/lib/find-content-by-id.ts
+++ src/lib/find-content-by-id.ts
@@ -1,10 +1,25 @@
-import { FullCourseContent } from '@/db/course';
+import { ChildCourseContent, FullCourseContent } from '@/db/course';
 
+// findContentById
+// This is incorrect.
+// If the place you are in is a folder
+// It returns all the children
+// If the place you are in is a video
+// It returns an array with that video as the first element.
 export default function findContentById(
   contents: FullCourseContent[],
   ids: number[],
-) {
-  if (ids.length === 0) return contents;
+):
+  | {
+      folder: true;
+      value: ChildCourseContent[];
+    }
+  | {
+      folder: false;
+      value: ChildCourseContent;
+    }
+  | null {
+  if (ids.length === 0) return { folder: true, value: contents };
 
   const currentId = ids[0];
   const remainingIds = ids.slice(1);
@@ -15,10 +30,10 @@ export default function findContentById(
     return null;
   } else if (remainingIds.length === 0) {
     if (foundContent.type === 'folder') {
-      return foundContent.children;
+      return { folder: true, value: foundContent.children ?? [] }; // [Object]
     }
 
-    return [foundContent];
+    return { folder: false, value: foundContent };
   }
 
   return findContentById(foundContent?.children || [], remainingIds);
