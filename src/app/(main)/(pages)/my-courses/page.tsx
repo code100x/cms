c256f5f6837df30c758f33487a0c8f6e13da7e2d
hkirat <harkirat96@gmail.com>
2024-08-20 11:34:57 +0530

    Fixed build, redirect

--- src/app/(main)/(pages)/my-courses/page.tsx
+++ src/app/(main)/(pages)/my-courses/page.tsx
@@ -1,6 +1,14 @@
 import { MyCourses } from '@/components/MyCourses';
+import { Redirect } from '@/components/Redirect';
+import { getServerSession } from 'next-auth';
 
 export default async function MyCoursesPage() {
+  const session = await getServerSession();
+
+  if (!session?.user) {
+    return <Redirect to={'/'} />;
+  }
+
   return (
     <div className="flex h-screen flex-col">
       <h1 className="bg-background/6 top-0 flex items-center p-5 text-3xl backdrop-blur-lg">
