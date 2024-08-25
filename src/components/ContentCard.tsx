7f7f032f49c6c9b36c136511a2e74f4b1a0ed545
Akshit <akshitgaur2003@gmail.com>
2024-08-26 00:39:31 +0530

    Fix play button and duration overlap issue

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -28,8 +28,6 @@ export const ContentCard = ({
   bookmark?: Bookmark | null;
   contentDuration?: number;
 }) => {
-  // let image ;
-  // image = ""
   return (
     <div
       onClick={onClick}
@@ -40,11 +38,28 @@ export const ContentCard = ({
           <CheckCircle2 color="green" size={30} fill="lightgreen" />
         </div>
       )}
+
       {type === 'video' && (
-        <div className="text-blue-900g absolute bottom-12 right-2 z-10 rounded-md bg-zinc-900 p-1 px-2 font-semibold text-white">
-          {contentDuration && formatTime(contentDuration)}
-        </div>
+        <>
+          <div className="absolute bottom-12 right-2 z-10 rounded-md bg-zinc-900 p-1 px-2 font-semibold text-white">
+            {contentDuration && formatTime(contentDuration)}
+          </div>
+          <div className="relative overflow-hidden rounded-md">
+            <VideoThumbnail
+              title={title}
+              contentId={contentId ?? 0}
+              imageUrl=""
+              // imageUrl={
+              //   'https://d2szwvl7yo497w.cloudfront.net/courseThumbnails/video.png'
+              // }
+            />
+            <div className="absolute bottom-4 right-4 z-20 rounded-full border border-gray-700/60 bg-white p-2">
+              <Play size={16} color="#64748b" />
+            </div>
+          </div>
+        </>
       )}
+
       {type !== 'video' && (
         <div className="relative overflow-hidden rounded-md">
           <CardComponent
@@ -62,18 +77,6 @@ export const ContentCard = ({
           )}
         </div>
       )}
-      {type === 'video' && (
-        <div className="relative overflow-hidden rounded-md">
-          <VideoThumbnail
-            title={title}
-            contentId={contentId ?? 0}
-            imageUrl=""
-            // imageUrl={
-            //   'https://d2szwvl7yo497w.cloudfront.net/courseThumbnails/video.png'
-            // }
-          />
-        </div>
-      )}
 
       {bookmark !== undefined && contentId && (
         <div className="absolute left-1 top-4">
@@ -86,17 +89,13 @@ export const ContentCard = ({
           />
         </div>
       )}
+
       <div className="flex items-center justify-between p-4">
         <div className="space-y-2">
           <h3 className="text-bold text-lg tracking-normal">{title}</h3>
           <h4 className="text-bold text-sm tracking-normal text-[#64748B]">
             Posted on: 10 Aug 2024
           </h4>
-        </div>
-        <div className="hidden rounded-full border border-gray-700/60 p-4 lg:block">
-          <div className="rounded-full border border-[#64748b] p-2">
-            <Play size={15} color="#64748b" />
-          </div>
         </div>
       </div>
     </div>
