7dd0d634b353f41728a2041ca77358c6ba62fadb
S. Adithiya <93365795+Adithiya5Hub@users.noreply.github.com>
2024-08-27 17:59:41 +0530

    Update PrimaryButton.tsx
    Added gradient effect on hovering

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -11,7 +11,7 @@ export const PrimaryButton = ({
     <button
       onClick={onClick}
       type="button"
-      className="mb-2 me-2 w-full rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+      className="mb-2 me-2 w-full rounded-full bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-r hover:from-blue-600 hover:to-indigo-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
     >
       {children}
     </button>
