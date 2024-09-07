45b424cbf9f472b448f1af9c2f7c45487426e4d8
pruthviarj7714 <marganepruthviraj@gmail.com>
2024-09-07 16:35:33 +0530

    fixed changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,7 +15,7 @@ export default function AdminPage() {
         Admin Dashboard
       </h1>
 
-      <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
         <Link
           href="/admin/add-course"
           className="group flex h-48 w-48 transform flex-col items-center justify-center rounded-lg bg-green-400 shadow-lg transition hover:scale-105 hover:shadow-2xl"
