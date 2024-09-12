b6430cbaf830be43d33179d1ed91dbdf98a5ea33
mortale2004 <pawanmortale111@gmail.com>
2024-09-12 15:21:58 +0530

    fix: Greeting in the home page

--- src/app/(main)/(pages)/home/page.tsx
+++ src/app/(main)/(pages)/home/page.tsx
@@ -1,3 +1,4 @@
+import { Greeting } from '@/components/Greeting';
 import { MyCourses } from '@/components/MyCourses';
 import { Redirect } from '@/components/Redirect';
 import SearchBar from '@/components/search/SearchBar';
@@ -10,22 +11,11 @@ export default async function MyCoursesPage() {
     return <Redirect to={'/'} />;
   }
 
-  // Get the current hour
-  const currentHour = new Date().getHours();
-
-  // Determine the appropriate greeting based on the time of day
-  let greeting = 'Good Morning';
-  if (currentHour >= 12 && currentHour < 18) {
-    greeting = 'Good Afternoon';
-  } else if (currentHour >= 18 || currentHour < 5) {
-    greeting = 'Good Evening';
-  }
-
   return (
     <main className="flex flex-col gap-4 pb-16 pt-8">
       <div className="flex flex-col justify-between gap-4 lg:flex-row">
         <h1 className="text-wrap text-3xl font-extrabold capitalize tracking-tighter md:text-4xl">
-          {greeting} {session.user.name}
+          <Greeting /> {session.user.name}
         </h1>
         <SearchBar />
       </div>
