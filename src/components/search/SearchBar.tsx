89ac0c29128a4d1a5349cade540867455283e0a2
Golden Kumar <auenkumar64@gmail.com>
2024-09-12 16:35:29 +0530

    added ctrl k in search

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,30 +1,23 @@
 'use client';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Input } from '@/components/ui/input';
-import { SearchIcon, X } from 'lucide-react';
+import { SearchIcon } from 'lucide-react';
 import { TSearchedVideos } from '@/app/api/search/route';
 import { useRouter } from 'next/navigation';
-import useClickOutside from '@/hooks/useClickOutside';
 import VideoSearchCard from './VideoSearchCard';
 import VideoSearchInfo from './VideoSearchInfo';
 import { toast } from 'sonner';
 import VideoSearchLoading from './VideoSearchLoading';
+import { Button } from '../ui/button';
+import { Dialog, DialogContent } from '../ui/dialog';
 
-const SearchBar = ({ onCardClick }: { onCardClick?: () => void }) => {
+const ContentSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchedVideos, setSearchedVideos] = useState<
-    TSearchedVideos[] | null
-  >(null);
-  const [isInputFocused, setIsInputFocused] = useState<boolean>(false);
+  const [searchedVideos, setSearchedVideos] = useState<TSearchedVideos[]>([]);
   const [loading, setLoading] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   const router = useRouter();
-
-  const ref = useRef<HTMLDivElement>(null);
-  const searchInputRef = useRef<HTMLInputElement>(null);
-
-  useClickOutside(ref, () => {
-    setIsInputFocused(false);
-  });
 
   const fetchData = useCallback(async (searchTerm: string) => {
     setLoading(true);
@@ -48,31 +41,54 @@ const SearchBar = ({ onCardClick }: { onCardClick?: () => void }) => {
 
       return () => clearTimeout(timeoutId);
     }
-    setSearchedVideos(null);
+    setSearchedVideos([]);
   }, [searchTerm, fetchData]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value);
-  };
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      switch (event.code) {
+        case 'KeyK':
+          if (event.ctrlKey) {
+            event.preventDefault();
+            setDialogOpen(true);
+          }
+          break;
+        case 'ArrowDown':
+          event.preventDefault();
+          setSelectedIndex(
+            (prevIndex) => (prevIndex + 1) % searchedVideos.length,
+          );
+          break;
+        case 'ArrowUp':
+          event.preventDefault();
+          setSelectedIndex(
+            (prevIndex) =>
+              (prevIndex - 1 + searchedVideos.length) % searchedVideos.length,
+          );
+          break;
+        case 'Enter':
+          if (selectedIndex !== -1) {
+            event.preventDefault();
+            const {
+              id: videoId,
+              parentId,
+              parent,
+            } = searchedVideos[selectedIndex];
 
-  const clearSearchTerm = () => {
-    setSearchTerm('');
-  };
-
-  const handleCardClick = (videoUrl: string) => {
-    if (onCardClick !== undefined) {
-      onCardClick();
-    }
-    clearSearchTerm();
-    router.push(videoUrl);
-  };
-
-  const handleClearInput = () => {
-    clearSearchTerm();
-    if (searchInputRef.current) {
-      searchInputRef.current.focus();
-    }
-  };
+            if (parentId && parent) {
+              const courseId = parent.courses[0].courseId;
+              const videoUrl = `/courses/${courseId}/${parentId}/${videoId}`;
+              router.push(videoUrl);
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    };
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, [searchedVideos, selectedIndex]);
 
   const renderSearchResults = () => {
     if (searchTerm.length < 3) {
@@ -84,45 +100,66 @@ const SearchBar = ({ onCardClick }: { onCardClick?: () => void }) => {
     } else if (!searchedVideos || searchedVideos.length === 0) {
       return <VideoSearchInfo text="No videos found" />;
     }
-    return searchedVideos.map((video) => (
-      <VideoSearchCard
-        key={video.id}
-        video={video}
-        onCardClick={handleCardClick}
-      />
+    return searchedVideos.map((video, index) => (
+      <div
+        className={` ${index === selectedIndex && 'bg-blue-600/10 text-blue-600'}`}
+      >
+        <VideoSearchCard key={video.id} video={video} />
+      </div>
     ));
+  };
+  const handleClose = (open: boolean) => {
+    if (!open) {
+      setDialogOpen(false);
+      setSearchTerm('');
+    }
   };
 
   return (
-    <div
-      className="relative flex h-10 w-full items-center lg:w-[32vw]"
-      ref={ref}
-    >
-      {/* Search Input Bar */}
-      <SearchIcon className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform text-primary/80" />
-      <Input
-        placeholder="Search for videos..."
-        className="focus:ring-none rounded-lg border-none bg-primary/5 px-10 text-base focus:outline-none"
-        value={searchTerm}
-        onChange={handleInputChange}
-        onFocus={() => setIsInputFocused(true)}
-        ref={searchInputRef}
-      />
-      {searchTerm.length > 0 && (
-        <X
-          className="absolute right-3 top-1/2 h-4 w-4 -translate-y-1/2 transform cursor-pointer"
-          onClick={handleClearInput}
-        />
-      )}
-
-      {/* Search Results */}
-      {isInputFocused && searchTerm.length > 0 && (
-        <div className="absolute top-12 z-30 max-h-[40vh] w-full overflow-y-auto rounded-lg border-2 bg-background p-2 shadow-lg">
-          {renderSearchResults()}
-        </div>
-      )}
-    </div>
+    <>
+      <Dialog open={dialogOpen} onOpenChange={handleClose}>
+        <Button
+          variant="outline"
+          className="pr-2"
+          onClick={() => setDialogOpen(true)}
+        >
+          <div className="flex w-full items-center justify-between gap-2">
+            <div className="flex text-primary/80 lg:w-[32vw]">
+              <SearchIcon className="h-[1.2rem] w-[1.2rem]" />
+              <span className="px-2">Search for videos</span>
+            </div>
+            <kbd className="rounded-sm bg-white/15 p-1.5 text-xs leading-3">
+              Ctrl K
+            </kbd>
+          </div>
+          <div className="block md:hidden">
+            <SearchIcon className="h-[1.2rem] w-[1.2rem]" />
+          </div>
+        </Button>
+        <DialogContent className="max-w-2xl gap-0 p-0">
+          {/* Search Input Bar */}
+          <div className="flex items-center border-b px-4 py-2">
+            <SearchIcon className="h-[1.5rem] w-[1.5rem]" />
+            <Input
+              type="text"
+              placeholder="Type title"
+              className="border-none text-base shadow-none focus-visible:outline-none focus-visible:ring-0"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          {/* Search Results */}
+          <div>
+            {searchTerm.length > 0 && (
+              <div className="absolute top-12 z-30 max-h-[40vh] w-full overflow-y-auto rounded-lg border-2 bg-background p-2 shadow-lg">
+                {renderSearchResults()}
+              </div>
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
-export default SearchBar;
+export default ContentSearch;
--- src/components/search/VideoSearchCard.tsx
+++ src/components/search/VideoSearchCard.tsx
@@ -1,29 +1,23 @@
 import { TSearchedVideos } from '@/app/api/search/route';
 import { Play } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
-const VideoSearchCard = ({
-  video,
-  onCardClick,
-}: {
-  video: TSearchedVideos;
-  onCardClick: (videoUrl: string) => void;
-}) => {
+const VideoSearchCard = ({ video }: { video: TSearchedVideos }) => {
   const { id: videoId, parentId, parent } = video;
-
   if (parentId && parent) {
     const courseId = parent.courses[0].courseId;
     const videoUrl = `/courses/${courseId}/${parentId}/${videoId}`;
     return (
-      <div
+      <Link
+        href={videoUrl}
         className="flex cursor-pointer items-center gap-3 rounded-md px-3 py-2 hover:bg-blue-600/10 hover:text-blue-600"
-        onClick={() => onCardClick(videoUrl)}
       >
         <Play className="size-4" />
         <span className="w-4/5 truncate font-medium capitalize">
           {video.title}
         </span>
-      </div>
+      </Link>
     );
   }
 };
