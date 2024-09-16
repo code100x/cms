8c1153ee7e6a28e9e6bbaa27bf718dbf8b359fc2
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-09-16 09:15:20 +0300

    Fixed linting errors

--- src/app/admin/user/LogoutUser.tsx
+++ src/app/admin/user/LogoutUser.tsx
@@ -10,7 +10,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
+} from '@/components/ui/accordion';
 import { LogOut } from 'lucide-react';
 
 const LogoutUserComp = () => {
@@ -26,29 +26,37 @@ const LogoutUserComp = () => {
     toast.info(res.message || res.error);
   };
   return (
-
-    <div className='w-full h-full'>
-      <Accordion defaultValue='approve-comment' className='border-2 p-4 rounded-2xl' type="single" collapsible>
-        <AccordionItem className='border-none' value="approve-comment">
-          <AccordionTrigger className='p-6 text-lg lg:text-2xl font-bold'>
-            <div className='flex gap-4 flex-col' >
+    <div className="h-full w-full">
+      <Accordion
+        defaultValue="approve-comment"
+        className="rounded-2xl border-2 p-4"
+        type="single"
+        collapsible
+      >
+        <AccordionItem className="border-none" value="approve-comment">
+          <AccordionTrigger className="p-6 text-lg font-bold lg:text-2xl">
+            <div className="flex flex-col gap-4">
               <LogOut size={40} /> Logout User
             </div>
           </AccordionTrigger>
           <AccordionContent>
-            <form className='w-full h-full' onSubmit={handlLogout} ref={formRef}>
-              <div className="grid w-full grid-cols-1 md:grid-cols-7 rounded-lg  shadow-sm dark:border-gray-800">
-                <div className="col-span-1 lg:col-span-3 grid gap-2 p-6">
+            <form
+              className="h-full w-full"
+              onSubmit={handlLogout}
+              ref={formRef}
+            >
+              <div className="grid w-full grid-cols-1 rounded-lg shadow-sm dark:border-gray-800 md:grid-cols-7">
+                <div className="col-span-1 grid gap-2 p-6 lg:col-span-3">
                   <div className="text-sm font-medium leading-none text-gray-500 dark:text-gray-400">
                     Enter the information below to logout the user
                   </div>
                 </div>
 
-                <aside className='col-span-1 flex flex-col gap-6 lg:col-span-4 w-full'>
+                <aside className="col-span-1 flex w-full flex-col gap-6 lg:col-span-4">
                   <div className="">
                     <Label className="sr-only">EMAIL</Label>
                     <Input
-                      className="w-full h-14 px-2"
+                      className="h-14 w-full px-2"
                       id="email"
                       name="email"
                       placeholder="example@gmail.com"
@@ -60,7 +68,7 @@ const LogoutUserComp = () => {
                   <div className="">
                     <Label className="sr-only">Admin password</Label>
                     <Input
-                      className="w-full h-14 px-2"
+                      className="h-14 w-full px-2"
                       id="adminPassword"
                       name="adminPassword"
                       placeholder="Admin password"
@@ -71,14 +79,12 @@ const LogoutUserComp = () => {
                   </div>
                   <Button className="w-full lg:w-[20%]">Logout</Button>
                 </aside>
-
               </div>
             </form>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
     </div>
-
   );
 };
 
