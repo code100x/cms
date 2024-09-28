224ce83fee3680ec0aa870161d58ccc4251160d5
Arnab Paul <arnab.paul.1656@gmail.com>
2024-09-29 02:32:52 +0530

    fix(ui): change cursor to pointer on theme toggle hover for better UX

--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -13,12 +13,12 @@ export function SelectTheme({ text }: { text?: boolean }) {
           className={`flex items-center gap-2 rounded-lg p-3 text-center transition-all duration-300 hover:bg-blue-600/5 hover:text-blue-500`}
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         >
-          <Sun className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="size-6 rotate-0 scale-100 cursor-pointer transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute size-6 rotate-90 scale-0 cursor-pointer transition-all dark:rotate-0 dark:scale-100" />
         </div>
       ) : (
         <div
-          className={`flex items-center gap-2 rounded-lg text-center transition-all duration-300 hover:bg-blue-600/5 hover:text-blue-500`}
+          className={`flex cursor-pointer items-center gap-2 rounded-lg text-center transition-all duration-300 hover:bg-blue-600/5 hover:text-blue-500`}
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         >
           <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
