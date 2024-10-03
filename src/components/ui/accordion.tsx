be64c75f5ecfac60941adc90462ba8c6410d3b28
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-10-03 12:40:29 +0530

    bug: Content of Sidebar UI in not appropriate in fixed sequence #1348

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between py-4 transition-all [&[data-state=open]>svg]:rotate-180',
+        'flex w-full items-center justify-between py-4 text-left transition-all [&[data-state=open]>svg]:rotate-180',
         className,
       )}
       {...props}
