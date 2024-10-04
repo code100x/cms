38c5f40b5411e3409e47a50a20a60c8a6cb805cc
Souradip Dasgupta <dgsouro77@gmail.com>
2024-10-04 22:04:25 +0530

    updated the issue of bookmark not stored

--- src/components/bookmark/BookmarkList.tsx
+++ src/components/bookmark/BookmarkList.tsx
@@ -10,41 +10,56 @@ const BookmarkList = ({
   bookmarkData: TBookmarkWithContent[] | { error: string };
 }) => {
   const router = useRouter();
+
+  // Handle error state
   if ('error' in bookmarkData) {
     return (
-      <h1 className="p-4">Something went wrong, please try again later</h1>
+      <h1 className="p-4 text-red-500">
+        Something went wrong, please try again later.
+      </h1>
     );
   }
-  return (
-    <>
-      <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {bookmarkData.map((bookmark) => {
-          const {
-            contentId,
-            content: { type, parent, title, hidden, thumbnail },
-          } = bookmark;
-          if (type === 'video' && parent && !hidden) {
-            const { id: folderId, courses } = parent;
-            const courseId = courses[0].courseId;
-            const videoUrl = `/courses/${courseId}/${folderId}/${contentId}`;
 
-            return (
-              <ContentCard
-                type={type}
-                key={contentId}
-                title={title}
-                image={thumbnail || ''}
-                onClick={() => {
-                  router.push(videoUrl);
-                }}
-                bookmark={bookmark}
-                contentId={contentId}
-              />
-            );
-          }
-        })}
-      </section>
-    </>
+  // Handle case where there are no bookmarks
+  if (bookmarkData.length === 0) {
+    return (
+      <h2 className="p-4">No bookmarks available. Start adding some!</h2>
+    );
+  }
+
+  return (
+    <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {bookmarkData.map((bookmark) => {
+        const {
+          contentId,
+          content: { type, parent, title, hidden, thumbnail },
+        } = bookmark;
+
+        // Only render bookmarks that are videos and not hidden
+        if (type === 'video' && parent && !hidden) {
+          const { id: folderId, courses } = parent;
+          const courseId = courses[0].courseId; // Assuming at least one course exists
+          const videoUrl = `/courses/${courseId}/${folderId}/${contentId}`;
+
+          return (
+            <ContentCard
+              type={type}
+              key={contentId} // Ensure each item has a unique key
+              title={title}
+              image={thumbnail || ''}
+              onClick={() => {
+                router.push(videoUrl);
+              }}
+              bookmark={bookmark}
+              contentId={contentId}
+            />
+          );
+        }
+
+        // Return null if the bookmark does not meet the criteria
+        return null; 
+      })}
+    </section>
   );
 };
 
