0fc8cd889dce38ffdc7b2b994dadbd522541e700
Prashant Kumar <prashantkumar1251@gmail.com>
2024-10-05 03:06:05 +0530

    fixed watch history bug and lint.yml

--- src/actions/payoutMethods/schema.ts
+++ src/actions/payoutMethods/schema.ts
@@ -1,15 +1,18 @@
 import { z } from 'zod';
 
+const UPI_REGEX = new RegExp('^[0-9A-Za-z._-]{2,256}@[A-Za-z]{2,64}$');
+const SOLANA_ADDRESS_REGEX = new RegExp('^[A-Za-z0-9]{44}$');
+
 export const payoutMethodSchema = z.object({
   upiId: z
     .string()
-    .refine((value) => (/^[0-9A-Za-z._-]{2,256}@[A-Za-z]{2,64}$/).test(value), {
+    .refine((value) => UPI_REGEX.test(value), {
       message: 'Enter a valid UPI address',
     })
     .optional(),
   solanaAddress: z
     .string()
-    .refine((value) => (/^[A-Za-z0-9]{44}$/).test(value), {
+    .refine((value) => SOLANA_ADDRESS_REGEX.test(value), {
       message: 'Enter a valid Solana address',
     })
     .optional(),
@@ -18,13 +21,13 @@ export const payoutMethodSchema = z.object({
 export const upiIdInsertSchema = z.object({
   upiId: z
     .string()
-    .refine((value) => (/^[0-9A_Za-z._-]{2,256}@[A_Za-z]{2,64}$/).test(value), {
+    .refine((value) => UPI_REGEX.test(value), {
       message: 'Invalid UPI address',
     }),
 });
 
 export const solanaAddressInsertSchema = z.object({
-  solanaAddress: z.string().refine((value) => (/^[A-Za-z0-9]{44}$/).test(value), {
+  solanaAddress: z.string().refine((value) => SOLANA_ADDRESS_REGEX.test(value), {
     message: 'Invalid Solana address',
   }),
 });
