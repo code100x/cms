5177cb19690f760c0509447a74fca3838fb82a55
SahilLamba0008 <19bcs1922@gmail.com>
2024-10-26 10:38:54 +0530

    resolved footer conflicts & commented un-used icons

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -45,28 +45,7 @@ const Footer = () => {
                   href={'https://x.com/kirat_tw'}
                   className="rounded-lg bg-blue-500/10 p-2 text-primary"
                 >
-<<<<<<< HEAD
-                  <svg
-                    viewBox="-0.5 -0.5 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="size-6 transition-all duration-300 hover:text-blue-600"
-                  >
-                    <path
-                      d="M11.133375000000001 14.11 1.2831875000000001 1.445375c-0.23112500000000002 -0.2970625 -0.019375 -0.73 0.35700000000000004 -0.73h1.869375c0.1395625 0 0.2713125 0.0644375 0.35700000000000004 0.17462499999999997l9.85025 12.6645625c0.23112500000000002 0.2971875 0.019375 0.7300625 -0.35700000000000004 0.7300625h-1.8693125c-0.139625 0 -0.271375 -0.0644375 -0.357125 -0.17462499999999997Z"
-                      stroke="currentColor"
-                      strokeWidth="1"
-                    ></path>
-                    <path
-                      d="M13.530750000000001 0.7153750000000001 1.46925 14.284625"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeWidth="1"
-                    ></path>
-                  </svg>
-=======
                   <XLogo />
->>>>>>> origin/main
                 </Link>
 
                 {/* Instagram Button */}
