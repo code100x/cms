5d79c6f00da24a5ce3646f9b44a1d684f057edcf
Harman Batheja <harmanbatheja15@gmail.com>
2024-10-29 10:38:33 +0530

    added bookmark endpoint for mobile app

---
+++ src/app/api/mobile/bookmarks/route.ts
@@ -0,0 +1,49 @@
+import db from '@/db';
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  try {
+    const user = JSON.parse(req.headers.get('g') || '');
+    const userId = user?.id;
+    if (!user) {
+      return NextResponse.json({ message: 'User Not Found' }, { status: 400 });
+    }
+    const userBookmarks = await db.bookmark.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        content: {
+          include: {
+            parent: {
+              select: {
+                id: true,
+                courses: true,
+              },
+            },
+            courses: {
+              select: {
+                courseId: true,
+                contentId: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return NextResponse.json({
+      message: 'User bookmarks fetched successfully',
+      data: userBookmarks,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: 'Error fetching user bookmarks', error },
+      { status: 500 },
+    );
+  }
+}
