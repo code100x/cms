912c83b8869e0f29e3f0b50bb41ede2aefe6a37b
Sargam Poudel <sargampoudel100@gmail.com>
2024-12-07 02:13:06 +0530

    cjsdlkf

--- src/middleware.ts
+++ src/middleware.ts
@@ -80,7 +80,6 @@ export async function middleware(req: NextRequestWithAuth) {
   if (pathname.startsWith('/api/mobile')) {
     return withMobileAuth(req);
   }
-  console.log('pathname', pathname);
   return await withAuth(req);
 }
 
