6cbc2b711b076a7dc7c5641a40ccd2bf20097b46
Rajveer Singh <rajveergreets@gmail.com>
2024-12-11 23:34:43 +0530

    Resolved logo and search icon overlap in the navbar on small mobile devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -80,7 +80,7 @@ export const Navbar = () => {
                 className="size-10 rounded-full"
               />
               <p
-                className={`hidden bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-2xl font-black tracking-tighter text-transparent min-[375px]:block`}
+                className={`hidden bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-2xl font-black tracking-tighter text-transparent min-[410px]:block`}
               >
                 100xDevs
               </p>
