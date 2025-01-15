3643626a2a723415bcb28acbd673a17793044990
ToseebNadaf <toseebnadaf@gmail.com>
2025-01-15 18:28:27 +0530

    Added missing authentication check

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { sendMail } from "@/lib/nodemailer";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session?.user) {
+    return NextResponse.json({error: "Unauthorized"}, { status: 401 });
+  }
+
   const body = await req.json();
   const { name, email, message } = body;
 
